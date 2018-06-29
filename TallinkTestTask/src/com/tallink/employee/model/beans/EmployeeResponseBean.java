9c5216b4ac6bdffbd952a5eff18bd6f9ec9bfe1d
KonashenkovKonstantin <kkonashenkov@gmail.com>
2018-06-29 17:36:45 +0300

    added tests

--- TallinkTestTask/src/com/tallink/employee/model/beans/EmployeeResponseBean.java
+++ TallinkTestTask/src/com/tallink/employee/model/beans/EmployeeResponseBean.java
@@ -12,6 +12,7 @@ public class EmployeeResponseBean {
 	@SerializedName("text")
 	private String employeeLabel;
 		
+	private long managerId;
 	private String firstName;	
 	private String secondName;
 	
@@ -20,6 +21,7 @@ public class EmployeeResponseBean {
 	
 	public EmployeeResponseBean(EmployeeData employeeData) {
 		this.employeeId = employeeData.getEmployeeId();
+		this.managerId = employeeData.getManagerId();
 		this.firstName = employeeData.getFirstName();
 		this.secondName = employeeData.getSecondName();
 		this.employeeLabel = getEmployerLabel();
@@ -33,6 +35,46 @@ public class EmployeeResponseBean {
 		childrens.add(employeeResponseBean);
 	}
 	
+	
+	
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public long getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(long managerId) {
+		this.managerId = managerId;
+	}
+
+	public long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public List<EmployeeResponseBean> getChildrens() {
+		return childrens;
+	}
+
 	public String getEmployerLabel() {
 		return this.firstName + " / " + this.secondName; 
 	}
---
+++ TallinkTestTask/test/junit/EmployeeServiceImplTest.java
@@ -0,0 +1,170 @@
+package junit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import com.tallink.employee.model.beans.EmployeeRequestBean;
+import com.tallink.employee.model.beans.EmployeeResponseBean;
+import com.tallink.employee.services.ServiceLocator;
+
+public class EmployeeServiceImplTest {
+
+	@Test
+	public void getEmployeesTest() {
+		try {
+			EmployeeResponseBean allEmployees = ServiceLocator.getEmployeeService().getAllEmployees();
+			//we are 100% sure that there are several employee
+			assertNotEquals(null, allEmployees);
+			assertNotEquals(null, allEmployees.getChildrens());
+			assertNotEquals(0, allEmployees.getChildrens().size());
+		} catch(Exception ee) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void addEmployeeTest() {
+		try {
+			
+			//1. prepare data
+			EmployeeResponseBean responseBean = ServiceLocator.getEmployeeService().getAllEmployees();
+			
+			long newEmployeeManagerId = getIdOfAnyEmployee(responseBean);
+			String newEmployeeFirstName = "TestFirstName";
+			String newEmployeeSecondName = "TestSecondName";
+			EmployeeRequestBean newEmployee = new EmployeeRequestBean();
+			newEmployee.setManagerId(newEmployeeManagerId);
+			newEmployee.setFirstName(newEmployeeFirstName);
+			newEmployee.setSecondName(newEmployeeSecondName);
+			assertEquals(false, containsEmployee(responseBean, newEmployee));
+			
+			//2. add new employee
+			ServiceLocator.getEmployeeService().addEmployee(newEmployee);
+			
+			//3. check that new employee has been added
+			responseBean = ServiceLocator.getEmployeeService().getAllEmployees();
+			assertEquals(false, containsEmployee(responseBean, newEmployee));			
+			
+			
+		} catch(Exception ee) {
+			fail();
+		}
+		
+	}
+	
+	@Test
+	public void deleteEmployeeTest() {
+		try {
+			//1. prepare data
+			EmployeeResponseBean responseBean = ServiceLocator.getEmployeeService().getAllEmployees();
+			long toDeleteEmployeeId = findAnyNotCeoEmployee(responseBean);
+			
+			EmployeeRequestBean employeeRequestBean = new EmployeeRequestBean();
+			employeeRequestBean.setEmployeeId(toDeleteEmployeeId);
+			
+			//2. delete
+			ServiceLocator.getEmployeeService().deleteEmployee(employeeRequestBean);
+			
+			//3. check that there is no that employee anymore
+			assertEquals(false, containsEmployee(responseBean, employeeRequestBean));		
+			
+		} catch(Exception ee) {			
+			fail();
+		}
+		
+	}
+	
+	@Test
+	public void deleteCEOEmployeeTest() {
+		try {
+			//1. prepare data
+			EmployeeResponseBean responseBean = ServiceLocator.getEmployeeService().getAllEmployees();
+			long toDeleteCeoEmployeeId = getIdOfCeoEmployee(responseBean);
+			
+			EmployeeRequestBean employeeRequestBean = new EmployeeRequestBean();
+			employeeRequestBean.setEmployeeId(toDeleteCeoEmployeeId);
+			//2. delete
+			ServiceLocator.getEmployeeService().deleteEmployee(employeeRequestBean);
+			fail("we have to get an error here");
+		} catch(Exception ee) {
+			
+		}
+		
+	}
+	
+	private long getIdOfAnyEmployee(EmployeeResponseBean employeeResponseBean) {
+		return employeeResponseBean.getEmployeeId();
+	}
+	
+	private long getIdOfCeoEmployee(EmployeeResponseBean employeeResponseBean) {
+		if (employeeResponseBean.getManagerId() == 0) {
+			return employeeResponseBean.getEmployeeId();
+		}
+		
+		if (employeeResponseBean.getChildrens() != null && employeeResponseBean.getChildrens().size() != 0) {
+			for (EmployeeResponseBean bean : employeeResponseBean.getChildrens()) {
+				long result = getIdOfCeoEmployee(bean);
+				if (result != -1) {
+					return result;
+				}
+			}
+		} 
+		return -1;
+	}
+	
+	private long findAnyNotCeoEmployee(EmployeeResponseBean employeeResponseBean) {
+		if (employeeResponseBean.getManagerId() != 0) {
+			return employeeResponseBean.getEmployeeId();
+		}
+		
+		if (employeeResponseBean.getChildrens() != null && employeeResponseBean.getChildrens().size() != 0) {
+			for (EmployeeResponseBean bean : employeeResponseBean.getChildrens()) {
+				long result = findAnyNotCeoEmployee(bean);
+				if (result != -1) {
+					return result;
+				}
+			}
+		} 
+		return -1;
+	}
+	
+	//rename parameters
+	private boolean containsEmployee(EmployeeResponseBean employeeResponseBean, EmployeeRequestBean employeeRequestBean) {
+		if (employeeResponseBean.getManagerId() == employeeRequestBean.getManagerId() &&
+				employeeResponseBean.getEmployeeId() == employeeRequestBean.getEmployeeId() &&
+				employeeResponseBean.getFirstName().equals(employeeRequestBean.getFirstName()) &&
+				employeeResponseBean.getSecondName().equals(employeeRequestBean.getSecondName())) {
+			return true;
+		}
+		
+		if (employeeResponseBean.getChildrens() != null && employeeResponseBean.getChildrens().size() != 0) {
+			for (EmployeeResponseBean bean : employeeResponseBean.getChildrens()) {
+				boolean result = containsEmployee(bean, employeeRequestBean);
+				if (result) {
+					return result;
+				}
+			}
+		} 
+		return false;
+	}
+	
+	private boolean containsEmployee(EmployeeResponseBean employeeResponseBean, long employeeId) {
+		if (employeeResponseBean.getEmployeeId() == employeeId) {
+			return true;
+		}
+		
+		if (employeeResponseBean.getChildrens() != null && employeeResponseBean.getChildrens().size() != 0) {
+			for (EmployeeResponseBean bean : employeeResponseBean.getChildrens()) {
+				boolean result = containsEmployee(bean, employeeId);
+				if (result) {
+					return result;
+				}
+			}
+		} 
+		return false;
+	}
+
+}
